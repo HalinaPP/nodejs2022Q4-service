296f7b58ec368766d2333e17000bd176fcd6fb49
Halina Pishchala <halina_pishchala@epam.com>
2023-02-06 19:06:24 +0300

    feat: get port from env

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { PORT } from './config';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -5,6 +6,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000);
+  await app.listen(PORT);
 }
 bootstrap();
